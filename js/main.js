ce6116003f0d55801119663dbfcebc17b4d9394b
sigmarius <aamokeeva@yandex.ru>
2020-11-07 18:44:47 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -4,23 +4,22 @@
 (function () {
   var pageHeader = document.querySelector('.page-header');
   var headerToggle = document.querySelector('.page-header__toggle');
-  var heroImage = document.querySelector('.hero__image');
+  var heroImage = document.querySelector('.hero img');
 
   if (pageHeader && headerToggle && heroImage) {
     pageHeader.classList.remove('page-header--nojs');
     pageHeader.classList.remove('page-header--opened');
-    heroImage.classList.remove('hero__image--moved');
     pageHeader.classList.add('page-header--closed');
 
     headerToggle.addEventListener('click', function () {
       if (pageHeader.classList.contains('page-header--closed')) {
         pageHeader.classList.remove('page-header--closed');
         pageHeader.classList.add('page-header--opened');
-        heroImage.classList.add('hero__image--moved');
+        heroImage.style.top = '240px';
       } else {
         pageHeader.classList.add('page-header--closed');
         pageHeader.classList.remove('page-header--opened');
-        heroImage.classList.remove('hero__image--moved');
+        heroImage.style.top = '0px';
       }
     });
   }

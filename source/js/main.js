86a5a4fbb871ed1f6b17e8978bc1055b5cb238a3
sigmarius <aamokeeva@yandex.ru>
2020-11-08 16:15:36 +0300

    Исправляет меню чтобы оно не сдвигало контент

--- source/js/main.js
+++ source/js/main.js
@@ -2,24 +2,26 @@
 
 // мобильное меню-гамбургер
 (function () {
+  // корректирует расположение текста в секции HERO, если JS загрузится
+  var HERO_TEXT_PADDING = '79px 18px 235px';
+
   var pageHeader = document.querySelector('.page-header');
   var headerToggle = document.querySelector('.page-header__toggle');
-  var heroImage = document.querySelector('.hero img');
+  var heroText = document.querySelector('.hero__wrapper');
 
-  if (pageHeader && headerToggle && heroImage) {
+  if (pageHeader && headerToggle && heroText) {
     pageHeader.classList.remove('page-header--nojs');
     pageHeader.classList.remove('page-header--opened');
     pageHeader.classList.add('page-header--closed');
+    heroText.style.padding = HERO_TEXT_PADDING;
 
     headerToggle.addEventListener('click', function () {
       if (pageHeader.classList.contains('page-header--closed')) {
         pageHeader.classList.remove('page-header--closed');
         pageHeader.classList.add('page-header--opened');
-        heroImage.style.top = '240px';
       } else {
         pageHeader.classList.add('page-header--closed');
         pageHeader.classList.remove('page-header--opened');
-        heroImage.style.top = '0px';
       }
     });
   }

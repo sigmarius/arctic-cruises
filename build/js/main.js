c47664f664825db8088e28dbae212ca2d6a24a62
sigmarius <aamokeeva@yandex.ru>
2020-11-15 17:12:03 +0300

    Исправляет ошибки после тестирования

--- build/js/main.js
+++ build/js/main.js
@@ -2,9 +2,6 @@
 
 // мобильное меню-гамбургер
 (function () {
-  // корректирует расположение текста в секции HERO, если JS загрузится
-  var HERO_TEXT_PADDING = '79px 18px 235px';
-
   var pageHeader = document.querySelector('.page-header');
   var headerToggle = document.querySelector('.page-header__toggle');
   var heroText = document.querySelector('.hero__wrapper');
@@ -13,7 +10,6 @@
     pageHeader.classList.remove('page-header--nojs');
     pageHeader.classList.remove('page-header--opened');
     pageHeader.classList.add('page-header--closed');
-    heroText.style.padding = HERO_TEXT_PADDING;
 
     headerToggle.addEventListener('click', function () {
       if (pageHeader.classList.contains('page-header--closed')) {
